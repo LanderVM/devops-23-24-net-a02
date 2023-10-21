1f2fdf60ad2fbb22d6debbb3cd148db5db99444e
Maurice Cantaert <maurice.cantaert@student.hogent.be>
2023-10-21 19:48:33 +0200

    Continue work database connection
    add correct builder and connection options in Api Program.cs
    add DomainContext with Formulas and Equipments
    add database gets created if it doesnt exist with initial seed
    add initial migration

---
+++ src/Api/Data/Extensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Api.Data;
+
+public static class Extensions
+{
+  public static void CreateDbIfNotExists(this IHost host)
+  {
+    {
+      using (var scope = host.Services.CreateScope())
+      {
+        var services = scope.ServiceProvider;
+        var context = services.GetRequiredService<DomainContext>();
+        context.Database.EnsureCreated();
+        DbInitializer.Initialize(context);
+      }
+    }
+  }
+}
--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,6 +1,52 @@
+using Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var dbConnection = "server=localhost;user=root;password=ewdjProject$$2;database=devopsA02";
+// new MariaDbServerVersion
+// new MySqlServerVersion(new Version(8, 0, 34));
+var serverVersion = ServerVersion.AutoDetect(dbConnection);
+
+builder.Services.AddDbContext<DomainContext>(
+  dbContextOptions =>
+    /*{
+      if (builder.Environment.IsDevelopment())
+        dbContextOptions.UseMySql(dbConnection, serverVersion)
+          .LogTo(Console.WriteLine, LogLevel.Information)
+          .EnableSensitiveDataLogging()
+          .EnableDetailedErrors();
+      else
+        dbContextOptions.UseMySql(dbConnection, serverVersion);
+    }*/
+    dbContextOptions
+      .UseMySql(dbConnection, serverVersion)
+      .LogTo(Console.WriteLine, LogLevel.Information)
+      .EnableSensitiveDataLogging()
+      .EnableDetailedErrors()
+);
+
+// TODO service for CRUD here
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+
+// TODO authorization
+// app.UseAuthorization();
+
+// TODO rest api controllers
+
+app.CreateDbIfNotExists();
+
+app.MapGet("/", () => "Navigate to /swagger for Swagger test UI");
 
 app.Run();
