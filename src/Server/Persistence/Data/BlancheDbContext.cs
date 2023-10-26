f8ff4c046ef38b5b9efcb66e11a9595c6edd4f02
Maurice Cantaert <maurice.cantaert@student.hogent.be>
2023-10-26 17:56:26 +0200

    Clean up code
    move database Context options to BlancheDbContext onConfiguring()
    change appsetings data to placeholder

--- src/Server/Persistence/Data/BlancheDbContext.cs
+++ src/Server/Persistence/Data/BlancheDbContext.cs
@@ -5,7 +5,24 @@ namespace Api.Data;
 
 public class BlancheDbContext : DbContext
 {
-  public BlancheDbContext(DbContextOptions<BlancheDbContext> options) : base(options) { }
+  private readonly IConfiguration Configuration;
+
+  public BlancheDbContext(DbContextOptions<BlancheDbContext> options, IConfiguration configuration) : base(options)
+  {
+    Configuration = configuration;
+  }
+
+  protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+  {
+    
+  var dbConnection = Configuration.GetConnectionString("DBConnectionString");
+  var serverVersion = ServerVersion.AutoDetect(dbConnection);
+  
+    dbContextOptionsBuilder.UseMySql(dbConnection, serverVersion)
+      .LogTo(Console.WriteLine, LogLevel.Information)
+      .EnableSensitiveDataLogging()
+      .EnableDetailedErrors();
+  }
 
   protected override void OnModelCreating(ModelBuilder modelBuilder)
   {
--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -1,9 +1,6 @@
 using Api.Data;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 // builder.Services.AddControllersWithViews(); TODO check for difference
 builder.Services.AddControllers();
@@ -11,31 +8,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddRazorPages();
 
-// TODO move sensitive data to environment
-// var dbConnection = "server=172.168.1.10;user=vagrant;password='ewdjProject$$2';database=devopsA02";
-var dbConnection = "server=localhost;user=root;password='ewdjProject$$2';database=devopsA02";
-var serverVersion = ServerVersion.AutoDetect(dbConnection);
-  
-builder.Services.AddDbContext<BlancheDbContext>(
-  dbContextOptions =>
-  {
-    if (builder.Environment.IsDevelopment())
-    {
-      dbContextOptions.UseMySql(dbConnection, serverVersion)
-        .LogTo(Console.WriteLine, LogLevel.Information)
-        .EnableSensitiveDataLogging()
-        .EnableDetailedErrors();
-    }
-    else
-    {
-      dbContextOptions.UseMySql(dbConnection, serverVersion);
-    }
-  }
-);
+builder.Services.AddDbContext<BlancheDbContext>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
   app.UseWebAssemblyDebugging();
