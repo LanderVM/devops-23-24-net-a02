335d8bff918c682748eee5abe395e6547cd290ef
LanderVM <lander.vanmolle@student.hogent.be>
2023-10-27 19:30:07 +0200

    Fixed bug, cleaned up project , added MudBlazor

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,11 +1,14 @@
-using Microsoft.AspNetCore.Components.Web;
+﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using devops_23_24_net_a02.Client;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
