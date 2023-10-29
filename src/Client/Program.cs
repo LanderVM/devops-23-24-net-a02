3542e52fedca13cbb1bc8716d8d3f0aea7c35b66
Kilian Ostijn <kilian.ostijn@student.hogent.be>
2023-10-29 20:08:13 +0100

    error gone

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -10,6 +10,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddMudServices(options =>
+{
+  options.PopoverOptions.ThrowOnDuplicateProvider = false;
+});
 
 builder.Services.AddMudServices();
 
