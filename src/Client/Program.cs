b8aa52ebc1b3c7fa8adc85086b00d04c585eecab
SanderGeuens <sander.geuens@student.hogent.be>
2023-11-08 22:45:18 +0100

    implementeren webshop voor extra materiaal

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -12,6 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<PersonalDetailsState>();
+builder.Services.AddScoped<ExtraMaterialState>();
 builder.Services.AddMudServices();
 builder.Services.AddMudServices(options =>
 {
