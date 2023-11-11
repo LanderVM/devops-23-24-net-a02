7a1f0eeff3b71c18e16bdfcc88350bb2f4f16f4a
LanderVM <lander.vanmolle@student.hogent.be>
2023-11-11 17:17:48 +0100

    Added FakeAuth

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -4,6 +4,8 @@ using devops_23_24_net_a02.Client;
 using MudBlazor.Services;
 using devops_23_24_net_a02.Client.Pages.ExtraMaterial;
 using shared.Equipment;
+using Microsoft.AspNetCore.Components.Authorization;
+using Project.Client.Shared;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -22,5 +24,9 @@ builder.Services.AddMudServices(options =>
 builder.Services.AddMudServices();
 
 builder.Services.AddScoped<IEquipmentService, EquipmentService>();
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<FakeAuthenticationProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<FakeAuthenticationProvider>());
 
 await builder.Build().RunAsync();
