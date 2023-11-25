1664d5c5d38a367419ce397d8dd725407d65dd72
LanderVM <lander.vanmolle@student.hogent.be>
2023-11-25 23:25:25 +0100

    Pages Part 1

---
+++ src/Client/Shared/Routes.cs
@@ -0,0 +1,10 @@
+﻿namespace devops_23_24_net_a02.Client.Shared;
+public static class Routes
+{
+  public const string AboutUs = "/OverOns";
+  public const string EventDetails = "/Event/EventDetails";
+  public const string PersonalDetails = "/PersoonlijkeDetails";
+  public const string Overview = "/Overzicht";
+
+  public const string ExtraMaterial = "/extramaterial";
+}
