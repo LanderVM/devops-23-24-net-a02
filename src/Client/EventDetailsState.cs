dfdd158e84bf875a06dea0ba130aa0ea586bd401
SanderGeuens <100416548+SanderGeuens@users.noreply.github.com>
2023-11-27 18:46:14 +0100

    toevoegen keuze trippel bier event details scherm

--- src/Client/EventDetailsState.cs
+++ src/Client/EventDetailsState.cs
@@ -5,15 +5,16 @@ namespace devops_23_24_net_a02.Client;
 
 public class EventDetailsState
 {
-  public string Address { get; set; }
+  public string FormattedAddress { get; set; }
   public PlaceGeometry PlaceGeometry { get; set; }
   public string PlaceTitle { get; set; }
   public int NumberOfPeople { get; set; }
 
+  public bool HasTripleBeer = false;
+
   public DateRange dateRange = new DateRange(DateTime.Now.Date, DateTime.Now.AddDays(3).Date);
 
   public TimeSpan? startTime = new TimeSpan();
-
   public TimeSpan? endTime = new TimeSpan();
 
 }
