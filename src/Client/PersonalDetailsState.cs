9c1d18791c581996d7fdaa3cdf03ad15fe49c1b1
SanderGeuens <100416548+SanderGeuens@users.noreply.github.com>
2023-12-05 00:37:50 +0100

    werkend krijgen validatie personal details

--- src/Client/PersonalDetailsState.cs
+++ src/Client/PersonalDetailsState.cs
@@ -1,12 +1,29 @@
 ﻿using FluentValidation;
 using shared;
 using Shared.Customer;
+using devops_23_24_net_a02.Shared.Emails;
+using Shared.Common;
 
 namespace devops_23_24_net_a02.Client;
 
 public class PersonalDetailsState
 {
-  public CustomerDto.Create customer = new();
+  public CustomerDto.Create customer = new CustomerDto.Create { 
+    FirstName="",
+    LastName="",
+    PhoneNumber="",
+    VatNumber="",
+    Email= new EmailDto.Create { 
+     Email=""
+    },
+    BillingAddress = new AddressDto { 
+     Street ="",
+     City = "",
+     PostalCode = "",
+     HouseNumber = "",
+    }
+
+  };
 
   /*
   public PersonalDetailsClass PersonalDetailsObject = new();
