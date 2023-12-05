77444b58b075494ca15b1934e968be0405358f2f
SanderGeuens <100416548+SanderGeuens@users.noreply.github.com>
2023-12-05 21:14:56 +0100

    toevoegen EntityAlreadyExistst en RecordNotFound exceptions

---
+++ src/Domain/Excpetions/EntityAlreadyExistsException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace devops-23-24-net-a02.Domain.Excpetions
+{
+    internal class EntityAlreadyExistsException
+{
+}
+}
