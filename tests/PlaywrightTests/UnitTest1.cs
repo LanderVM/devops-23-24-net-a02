462ec20473b34ba2070d0be694c30b887417bf15
SanderGeuens <100416548+SanderGeuens@users.noreply.github.com>
2023-12-13 18:30:29 +0100

    adding playwright project to solution

---
+++ tests/PlaywrightTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Client.PlaywrightTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
