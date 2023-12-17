564cb49fa845278f041551de36cec4d96f5045b5
SanderGeuens <100416548+SanderGeuens@users.noreply.github.com>
2023-12-17 22:58:10 +0100

    adding extras admin test for updating extra

--- tests/PlaywrightTests/AdminExtraMaterialTest.cs
+++ tests/PlaywrightTests/AdminExtraMaterialTest.cs
@@ -5,7 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
+using NUnit.Framework.Internal;
 using Shouldly;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Client.PlaywrightTests;
 
@@ -91,13 +93,12 @@ public class AdminExtraMaterialTest:PageTest
     await Page.GotoAsync(TestHelper.ExtraMaterialAdmin);
     await Page.WaitForSelectorAsync("data-test-id=extras-admin-card-overview");
     await Page.WaitForSelectorAsync("data-test-id=extras-admin-extra-card");
-    //var amountOfCards = await Page.Locator("data-test-id=extras-admin-extra-card").CountAsync();
-    //amountOfCards.ShouldBeGreaterThan(0);
+    
     int initialAmount = await Page.Locator("data-test-id=extras-admin-extra-card").CountAsync();
 
     await Page.Locator("data-test-id=extras-admin-create-button").ClickAsync();
     await Page.Locator("data-test-id=admin-extra-create-title").FillAsync("Bucket");
-    await Page.Locator("data-test-id=admin-extra-create-price").FillAsync("5.55");
+    await Page.Locator("data-test-id=admin-extra-create-price").FillAsync("43.72");
     await Page.Locator("data-test-id=admin-extra-create-stock").FillAsync("4");
     await Page.Locator("data-test-id=admin-extra-create-attributes").FillAsync("spoon;soap");
     await Page.Locator("data-test-id=admin-extra-create-createbutton").ClickAsync();
@@ -108,5 +109,47 @@ public class AdminExtraMaterialTest:PageTest
     int eventualAmount = await Page.Locator("data-test-id=extras-admin-extra-card").CountAsync();
 
     eventualAmount.ShouldBe(initialAmount + 1);
+
+    await Page.GetByText("Bucket").IsVisibleAsync();
+    await Page.GetByText("43.72").IsVisibleAsync();
+  }
+
+  [Test]
+  public async Task GoToExtraMaterialAdminUpdateCard()
+  {
+    string email = ClientTestCredentials.email;
+    string password = ClientTestCredentials.password;
+
+    await Page.GotoAsync(TestHelper.BaseUri);
+    await Page.WaitForSelectorAsync("data-test-id=home-cards-overview");
+    await Page.WaitForSelectorAsync("data-test-id=admin-button");
+    await Page.Locator("data-test-id=admin-button").ClickAsync();
+
+    await Page.WaitForTimeoutAsync(7000);
+    await Page.GetByLabel("Email address").FillAsync(email);
+    await Page.GetByLabel("Password").FillAsync(password);
+    await Page.GetByRole(AriaRole.Button, new() { Name = "Continue" }).ClickAsync();
+
+    await Page.WaitForSelectorAsync("data-test-id=home-cards-overview");
+
+    await Page.GotoAsync(TestHelper.ExtraMaterialAdmin);
+    await Page.WaitForSelectorAsync("data-test-id=extras-admin-card-overview");
+    await Page.WaitForSelectorAsync("data-test-id=extras-admin-extra-card");
+
+    await Page.Locator("data-test-id=extras-admin-extra-card-update-button").Last.ClickAsync();
+
+    await Page.Locator("data-test-id=admin-extra-edit-title").FillAsync("Gun");
+    await Page.Locator("data-test-id=admin-extra-edit-price").FillAsync("23.66");
+    await Page.Locator("data-test-id=admin-extra-edit-stock").FillAsync("6");
+    await Page.Locator("data-test-id=admin-extra-edit-attributes").FillAsync("powder;bullets");
+
+    await Page.Locator("data-test-id=extras-admin-edit-editbutton").Last.ClickAsync();
+
+    await Page.GetByText("Gun").IsVisibleAsync();
+    await Page.GetByText("23.66").IsVisibleAsync();
   }
 }
+
+
+
+
