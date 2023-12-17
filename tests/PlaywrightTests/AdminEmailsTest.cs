563c87b4ce0c74614655c592ff69a8bafccbaa04
SanderGeuens <100416548+SanderGeuens@users.noreply.github.com>
2023-12-17 17:03:24 +0100

    make it possible to login in playwright tests without hardcoding credentials

--- tests/PlaywrightTests/AdminEmailsTest.cs
+++ tests/PlaywrightTests/AdminEmailsTest.cs
@@ -14,15 +14,18 @@ public class AdminEmailsTest : PageTest
   [Test]
   public async Task GoToEmailsOverviewCheckEmailsExists()
   {
-    DotNetEnv.Env.Load();
+    //DotNetEnv.Env.Load();
 
     //DotNetEnv.Env.Load();
 
-    string email = System.Environment.GetEnvironmentVariable("EMAIL");
-    string password = System.Environment.GetEnvironmentVariable("PASSWORD");
+    //string email = System.Environment.GetEnvironmentVariable("EMAIL");
+    //string password = System.Environment.GetEnvironmentVariable("PASSWORD");
 
     //string email = DotNetEnv.Env.GetString("EMAIL");
     //string password = DotNetEnv.Env.GetString("PASSWORD");
+
+    string email = ClientTestCredentials.email;
+    string password = ClientTestCredentials.password;
 
     await Page.GotoAsync(TestHelper.BaseUri);
     await Page.WaitForSelectorAsync("data-test-id=home-cards-overview");
@@ -30,8 +33,8 @@ public class AdminEmailsTest : PageTest
     
     await Page.Locator("data-test-id=admin-button").ClickAsync();
     await Page.WaitForTimeoutAsync(7000);
-    await Page.GetByLabel("Email address").FillAsync(email!);
-    await Page.GetByLabel("Password").FillAsync(password!);
+    await Page.GetByLabel("Email address").FillAsync(email);
+    await Page.GetByLabel("Password").FillAsync(password);
     
     await Page.GetByRole(AriaRole.Button, new() { Name = "Continue" }).ClickAsync();
     await Page.WaitForSelectorAsync("data-test-id=home-cards-overview");
