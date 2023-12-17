3c58e117ea940499003c65a02dfe3bb1817dd2f0
SanderGeuens <100416548+SanderGeuens@users.noreply.github.com>
2023-12-17 18:40:01 +0100

    adding extras admin tests for existing and remove

---
+++ tests/PlaywrightTests/AdminExtraMaterialTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using Shouldly;
+
+namespace Client.PlaywrightTests;
+
+public class AdminExtraMaterialTest:PageTest
+{
+  [Test]
+  public async Task GoToExtraMaterialAdminCheckExtraMaterialExists()
+  {
+    string email = ClientTestCredentials.email;
+    string password = ClientTestCredentials.password;
+
+    await Page.GotoAsync(TestHelper.BaseUri);
+    await Page.WaitForSelectorAsync("data-test-id=home-cards-overview");
+    await Page.WaitForSelectorAsync("data-test-id=admin-button");
+    await Page.Locator("data-test-id=admin-button").ClickAsync();
+
+    await Page.WaitForTimeoutAsync(7000);
+    await Page.GetByLabel("Email address").FillAsync(email);
+    await Page.GetByLabel("Password").FillAsync(password);
+    await Page.GetByRole(AriaRole.Button, new() { Name = "Continue" }).ClickAsync();
+
+    await Page.WaitForSelectorAsync("data-test-id=home-cards-overview");
+
+    await Page.GotoAsync(TestHelper.ExtraMaterialAdmin);
+    await Page.WaitForSelectorAsync("data-test-id=extras-admin-card-overview");
+    await Page.WaitForSelectorAsync("data-test-id=extras-admin-extra-card");
+    var amountOfCards = await Page.Locator("data-test-id=extras-admin-extra-card").CountAsync();
+    amountOfCards.ShouldBeGreaterThan(0);
+  }
+
+  [Test]
+  public async Task GoToExtraMaterialAdminRemoveCard()
+  {
+    string email = ClientTestCredentials.email;
+    string password = ClientTestCredentials.password;
+
+    await Page.GotoAsync(TestHelper.BaseUri);
+    await Page.WaitForSelectorAsync("data-test-id=home-cards-overview");
+    await Page.WaitForSelectorAsync("data-test-id=admin-button");
+    await Page.Locator("data-test-id=admin-button").ClickAsync();
+
+    await Page.WaitForTimeoutAsync(7000);
+    await Page.GetByLabel("Email address").FillAsync(email);
+    await Page.GetByLabel("Password").FillAsync(password);
+    await Page.GetByRole(AriaRole.Button, new() { Name = "Continue" }).ClickAsync();
+
+    await Page.WaitForSelectorAsync("data-test-id=home-cards-overview");
+
+    await Page.GotoAsync(TestHelper.ExtraMaterialAdmin);
+    await Page.WaitForSelectorAsync("data-test-id=extras-admin-card-overview");
+    await Page.WaitForSelectorAsync("data-test-id=extras-admin-extra-card");
+    //var amountOfCards = await Page.Locator("data-test-id=extras-admin-extra-card").CountAsync();
+    //amountOfCards.ShouldBeGreaterThan(0);
+    int initialAmount = await Page.Locator("data-test-id=extras-admin-extra-card").CountAsync();
+
+    await Page.Locator("data-test-id=extras-admin-extra-card-remove-button").Last.ClickAsync();
+    await Page.Locator("data-test-id=extras-admin-extra-card-remove-button-definitly").Last.ClickAsync();
+
+    await Page.WaitForTimeoutAsync(7000);
+
+    int eventualAmount = await Page.Locator("data-test-id=extras-admin-extra-card").CountAsync();
+
+    eventualAmount.ShouldBe(initialAmount-1);
+  }
+}
