853b134853d254bdf62772186c00f128226de42c
SanderGeuens <100416548+SanderGeuens@users.noreply.github.com>
2023-12-17 21:55:48 +0100

    expanding admin extra items tests

--- tests/PlaywrightTests/AdminExtraMaterialTest.cs
+++ tests/PlaywrightTests/AdminExtraMaterialTest.cs
@@ -57,8 +57,7 @@ public class AdminExtraMaterialTest:PageTest
     await Page.GotoAsync(TestHelper.ExtraMaterialAdmin);
     await Page.WaitForSelectorAsync("data-test-id=extras-admin-card-overview");
     await Page.WaitForSelectorAsync("data-test-id=extras-admin-extra-card");
-    //var amountOfCards = await Page.Locator("data-test-id=extras-admin-extra-card").CountAsync();
-    //amountOfCards.ShouldBeGreaterThan(0);
+    
     int initialAmount = await Page.Locator("data-test-id=extras-admin-extra-card").CountAsync();
 
     await Page.Locator("data-test-id=extras-admin-extra-card-remove-button").Last.ClickAsync();
@@ -69,5 +68,45 @@ public class AdminExtraMaterialTest:PageTest
     int eventualAmount = await Page.Locator("data-test-id=extras-admin-extra-card").CountAsync();
 
     eventualAmount.ShouldBe(initialAmount-1);
+  }
+
+  [Test]
+  public async Task GoToExtraMaterialAdminAddCard()
+  {
+    string email = ClientTestCredentials.email;
+    string password = ClientTestCredentials.password;
+
+    await Page.GotoAsync(TestHelper.BaseUri);
+    await Page.WaitForSelectorAsync("data-test-id=home-cards-overview");
+    await Page.WaitForSelectorAsync("data-test-id=admin-button");
+    await Page.Locator("data-test-id=admin-button").ClickAsync();
+
+    await Page.WaitForTimeoutAsync(7000);
+    await Page.GetByLabel("Email address").FillAsync(email);
+    await Page.GetByLabel("Password").FillAsync(password);
+    await Page.GetByRole(AriaRole.Button, new() { Name = "Continue" }).ClickAsync();
+
+    await Page.WaitForSelectorAsync("data-test-id=home-cards-overview");
+
+    await Page.GotoAsync(TestHelper.ExtraMaterialAdmin);
+    await Page.WaitForSelectorAsync("data-test-id=extras-admin-card-overview");
+    await Page.WaitForSelectorAsync("data-test-id=extras-admin-extra-card");
+    //var amountOfCards = await Page.Locator("data-test-id=extras-admin-extra-card").CountAsync();
+    //amountOfCards.ShouldBeGreaterThan(0);
+    int initialAmount = await Page.Locator("data-test-id=extras-admin-extra-card").CountAsync();
+
+    await Page.Locator("data-test-id=extras-admin-create-button").ClickAsync();
+    await Page.Locator("data-test-id=admin-extra-create-title").FillAsync("Bucket");
+    await Page.Locator("data-test-id=admin-extra-create-price").FillAsync("5.55");
+    await Page.Locator("data-test-id=admin-extra-create-stock").FillAsync("4");
+    await Page.Locator("data-test-id=admin-extra-create-attributes").FillAsync("spoon;soap");
+    await Page.Locator("data-test-id=admin-extra-create-createbutton").ClickAsync();
+    
+
+    await Page.WaitForTimeoutAsync(7000);
+
+    int eventualAmount = await Page.Locator("data-test-id=extras-admin-extra-card").CountAsync();
+
+    eventualAmount.ShouldBe(initialAmount + 1);
   }
 }
