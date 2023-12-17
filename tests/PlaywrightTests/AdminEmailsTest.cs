5daadfe1a2bbcdbc9bbbe6454a9300e52679eb83
SanderGeuens <100416548+SanderGeuens@users.noreply.github.com>
2023-12-17 16:54:21 +0100

    attempt at logging in in test with use of env file

--- tests/PlaywrightTests/AdminEmailsTest.cs
+++ tests/PlaywrightTests/AdminEmailsTest.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
 
 namespace Client.PlaywrightTests;
 
-public class AdminEmailsTest
+[Parallelizable(ParallelScope.Self)]
+public class AdminEmailsTest : PageTest
 {
+  [Test]
+  public async Task GoToEmailsOverviewCheckEmailsExists()
+  {
+    DotNetEnv.Env.Load();
+
+    //DotNetEnv.Env.Load();
+
+    string email = System.Environment.GetEnvironmentVariable("EMAIL");
+    string password = System.Environment.GetEnvironmentVariable("PASSWORD");
+
+    //string email = DotNetEnv.Env.GetString("EMAIL");
+    //string password = DotNetEnv.Env.GetString("PASSWORD");
+
+    await Page.GotoAsync(TestHelper.BaseUri);
+    await Page.WaitForSelectorAsync("data-test-id=home-cards-overview");
+    await Page.WaitForSelectorAsync("data-test-id=admin-button");
+    
+    await Page.Locator("data-test-id=admin-button").ClickAsync();
+    await Page.WaitForTimeoutAsync(7000);
+    await Page.GetByLabel("Email address").FillAsync(email!);
+    await Page.GetByLabel("Password").FillAsync(password!);
+    
+    await Page.GetByRole(AriaRole.Button, new() { Name = "Continue" }).ClickAsync();
+    await Page.WaitForSelectorAsync("data-test-id=home-cards-overview");
+  }
 
 }
