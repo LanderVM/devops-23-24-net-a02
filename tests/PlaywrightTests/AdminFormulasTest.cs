85655a8d4f7213e2a58a5e55c6bb5c569829f7a6
SanderGeuens <100416548+SanderGeuens@users.noreply.github.com>
2023-12-20 00:51:43 +0100

    expand admin formulas test

--- tests/PlaywrightTests/AdminFormulasTest.cs
+++ tests/PlaywrightTests/AdminFormulasTest.cs
@@ -34,8 +34,9 @@ public class AdminFormulasTest:PageTest
 
     await Page.GotoAsync(TestHelper.FormulasAdmin);
     await Page.WaitForSelectorAsync("data-test-id=formulas-admin-formulastable");
-    //int amount = await Page.Locator("data-test-id=formulas-admin-overview-editbutton").CountAsync();
-    //amount.ShouldBeGreaterThan(0);
+    await Page.WaitForSelectorAsync("data-test-id=formulas-admin-overview-editbutton");
+    int amount = await Page.Locator("data-test-id=formulas-admin-overview-editbutton").CountAsync();
+    amount.ShouldBe(3);
   }
 
   [Test]
@@ -58,7 +59,15 @@ public class AdminFormulasTest:PageTest
 
     await Page.GotoAsync(TestHelper.FormulasAdmin);
     await Page.WaitForSelectorAsync("data-test-id=formulas-admin-formulastable");
-    //await Page.Locator("data-test-id=formulas-admin-overview-editbutton").Last.ClickAsync();
+    await Page.WaitForSelectorAsync("data-test-id=formulas-admin-overview-editbutton");
+    await Page.Locator("data-test-id=formulas-admin-overview-editbutton").Last.ClickAsync();
     
+    await Page.Locator("data-test-id=formulas-admin-edit-title").FillAsync("Special offer");
+    await Page.Locator("data-test-id=formulas-admin-edit-pricePerDayExtra").FillAsync("24");
+
+    await Page.Locator("data-test-id=formulas-admin-edit-editbutton").ClickAsync();
+
+    await Page.GetByText("Special offer").IsVisibleAsync();
+    await Page.GetByText("24").IsVisibleAsync();
   }
 }
